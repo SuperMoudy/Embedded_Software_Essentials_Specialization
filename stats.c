4a24b1dc3f7635de66dc61220509e4792b341600
Mahmoud Hamdy <mahmoudhamdy.eeng@gmail.com>
2020-03-26 23:03:45 +0200

    Implementing code for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -43,42 +43,94 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+
+  /* Statistics and Printing Functions */
+  print_statistics(test, SIZE);
+  print_array(test, SIZE);
 
 }
 
 void print_statistics(unsigned char arr[], unsigned int size)
 {
+  unsigned char median = find_median(arr, size);
+  unsigned char mean = find_mean(arr, size);
+  unsigned char maximum = find_maximum(arr, size);
+  unsigned char minimum = find_minimum(arr, size);
 
+  printf("Median  = %3u\n", median);
+  printf("Mean    = %3u\n", mean);
+  printf("Maximum = %3u\n", maximum);
+  printf("Minimum = %3u\n\n", minimum); 
 }
 
 void print_array(unsigned char arr[], unsigned int size)
 {
-
+  for(int i = 0; i < size; i++)
+  {
+    printf("%3u ", arr[i]);
+  }
+  printf("\n\n");
 }
 
 unsigned char find_median(unsigned char arr[], unsigned int size)
 {
+  sort_array(arr, size);
+  int mid_index = size / 2;
 
+  if(size % 2 != 0)
+    return arr[mid_index];
+
+  return (arr[mid_index] + arr[mid_index - 1]) / 2;
 }
 
 unsigned char find_mean(unsigned char arr[], unsigned int size)
 {
+  int sum = 0;
+  for(int i = 0; i < size; i++)
+  {
+    sum += arr[i];
+  }
 
+  return sum / size;
 }
 
 unsigned char find_maximum(unsigned char arr[], unsigned int size)
 {
+  unsigned char max = arr[0];
+  for(int i = 0; i < size; i++)
+  {
+    if(arr[i] > max)
+      max = arr[i];
+  }
 
+  return max;
 }
 
 unsigned char find_minimum(unsigned char arr[], unsigned int size)
 {
+  unsigned char min = arr[0];
+  for(int i = 0; i < size; i++)
+  {
+    if(arr[i] < min)
+      min = arr[i];
+  }
 
+  return min;
 }
 
 void sort_array(unsigned char arr[], unsigned int size)
 {
-
+  unsigned char min, temp;
+  for(int i = 0; i < size - 1; i++)
+  {
+    for(int j = 0; j < size - 1 - i; j++)
+    {
+      if(arr[j] < arr[j + 1])
+      {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 }
