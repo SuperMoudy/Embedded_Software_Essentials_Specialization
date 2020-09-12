8d96b259d6a32ac8fd53a0a68d5b5873a8254917
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-09-12 10:18:08 +0200

    add course 1 - module 2

---
+++ Course1/Module2/src/sources.mk
@@ -0,0 +1,32 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+#*****************************************************************************
+
+ifeq ($(PLATFORM),HOST)
+	# Add your Source files to this variable
+	SOURCES = ./main.c ./memory.c
+
+	# Add your include paths to this variable
+	INCLUDES = -I../include/common
+else
+	# Add your Source files to this variable
+	SOURCES =	./main.c \
+				./memory.c \
+				./interrupts_msp432p401r_gcc.c \
+				./startup_msp432p401r_gcc.c \
+				./system_msp432p401r.c
+	
+	# Add your include paths to this variable
+	INCLUDES =  -I../include/CMSIS \
+				-I../include/common \
+				-I../include/msp432
+endif
+
+
